5d01ae91995b915eb68160ff025478c056eb9448
yiwei-ellen <yiguoyan0001@gmail.com>
2025-03-08 20:21:31 -0800

    Updates

---
+++ resume.pdf.tsx
@@ -0,0 +1,4 @@
+// This is a placeholder file to demonstrate the PDF link
+// In a real project, you would place an actual PDF file in the public folder
+// named "resume.pdf" instead of this TypeScript file
+
